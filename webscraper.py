3a259db433b9c2f06478bdf2f557071e003aa082
daniel.viernes <daniel.viernes@mynt.xyz>
2020-07-25 15:10:22 +0800

    changed to headless scraper

--- webscraper.py
+++ webscraper.py
@@ -1,23 +1,23 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
 from configuration import config
 from functools import lru_cache
+import requests
 
-driver = webdriver.Chrome(executable_path=r"C:/Development/tools/chromedriver/chromedriver.exe")
 
 @lru_cache(maxsize=32)
 def getValorantVersion():
-    driver.get(config.links["valorant-news"])
+    url = config.links["valorant-news"]
 
-    content = driver.page_source
-    soup = BeautifulSoup(content)
+    content = requests.get(url)
+    soup = BeautifulSoup(content.text, "html.parser")
+
+    print(soup.prettify()) #debug
 
     versions = []
     patchNotesTitleSubstr = 'VALORANT PATCH NOTES'
 
     for newsTitle in soup.findAll('h5', attrs={'class':'NewsCard-module--title--1MoLu'}):
-        print( newsTitle )
+        print( newsTitle ) #debug
         if( patchNotesTitleSubstr in newsTitle.text.upper() ):
             versions.append(newsTitle.text)
 
